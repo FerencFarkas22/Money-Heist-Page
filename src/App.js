ea33c7a4df51268726875af3ffd4cd52ef356e3e
FerencFarkas22 <85436006+FerencFarkas22@users.noreply.github.com>
2023-02-02 19:33:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+import { HashRouter as Router } from 'react-router-dom'
+import Header from './Components/Header'
+import Content from './Components/Content'
+import Home from './Pages/Home'
+import Actors from './Pages/Actors'
+import Story from './Pages/Story'
+import Contact from './Pages/Contact'
+import PageNotFound from './Pages/PageNotFound'
 
-function App() {
+const App = () => {
+    const pages = [
+        {name: "Kezdőlap", path: '/', menubar: true, element: <Home/>},
+        {name: "Szereplők", path: '/actors', menubar: true, element:<Actors/>},
+        {name: "Történet", path: '/story', menubar: true, element:<Story/>},
+        {name: "Kapcsolat", path: '/contact', menubar: true, element: <Contact/>},
+        {name: "notFound", path: "*", menubar:false, element:<PageNotFound/>}
+      ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <Router>
+            <Header menu={pages}/>
+            <Content routes={pages}/>
+        </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
