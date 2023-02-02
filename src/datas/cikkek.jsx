11f7549bc8a36e4601f6f74338d1bca7ce7366b0
FerencFarkas22 <85436006+FerencFarkas22@users.noreply.github.com>
2023-02-02 19:32:46 +0100

    first commit Datas

---
+++ src/datas/cikkek.jsx
@@ -0,0 +1,35 @@
+import madrid from '../assets/mardird.jpg'
+import home7 from '../assets/home7.jpg'
+import home6 from '../assets/home6.jpg'
+import home5 from '../assets/home5.jpg'
+export const cikkek = [
+  {
+    articleName: "Madrid jól megél a Nagy Pénzrablásból",
+    description: "Madrid neve az utóbbi években összeforrt a Netflix talán leghíresebb idegen nyelvű sorozatával, a La Casa de Papel (magyarul: A nagy pénzrablás) című műsorral, lévén, hogy ez a város adott otthont a történetnek. Az ötévados rablósztori – pontosabban a roppant szerethető bűnözők – elvarázsolták az egész világot, sőt talán még azt is ki lehet jelenteni, hogy visszatették a spanyol televízió- és filmipart a térképre.",
+    image: madrid,
+    buttonText: "Teljes cikk",
+    link: "https://index.hu/kultur/2022/01/23/madrid-megel-a-nagy-penzrablasbol-tokio-ursula-corbero-spanyolorszag-netflix-la-casa-de-papel/"
+  },
+  {
+    articleName: "A kulisszák mögött: minden, amit tudni akartál A nagy pénzrablásról",
+    description: "A nagy pénzrablás ma a legnézettebb sorozatok egyike. Sikerét többek között szerethető karakterei miatt érte el. A sorozat főszereplője, a kissé különc Professzor halálos betegséggel küzdött a forgatás előtt, de a Nagy pénzrablásban újjászülethetett, Nairobi pedig a való életben is egy igazi független, erős nő. A forgatáson a szereplőknek még a tengeribetegséggel is meg kellett küzdenie.",
+    image: home7,
+    buttonText: "Teljes cikk",
+    link: "https://metropol.hu/vip/2022/05/nagy-penzrablas-netflix-sorozat"
+  },
+  {
+    articleName: "Véget ért A nagy pénzrablás 5. évadának forgatása",
+    description: "A nagy pénzrablás egy igazi fenoménná vált az évek során. Ha nem lett volna a Netflix és annak is a világ összes pontjára elérő nagy keze, akkor az első évad után vége is lett volna a sorozatnak. Nem kapott anno nagy figyelmet, viszont miután a streamingplatform felkarolta és reklámozni kezdte, egy csapásra hatalmas siker lett. Milliós rajongótáborral, rengeteg megvehető árucikkel",
+    image: home6,
+    buttonText: "Teljes cikk",
+    link: "https://www.puliwood.hu/hirek/veget-ert-a-nagy-penzrablas-5-evadanak-forgatasa-294326.html"
+  },
+  {
+    articleName: "HIÁNYZIK A NAGY PÉNZRABLÁS? NOSZTALGIÁZZ KULISSZATITKOKKAL!",
+    description: "Bár Berlin, a sorozat egyik főszereplője jövőre spinoffot kap a Netflixtől, biztosan sokan vagyunk olyanok, akiknek máris nagyon hiányzik a spanyol sikersorozat. Hoztunk néhány érdekességet, hogy együtt emlékezhessünk a legszebb pillanatokra!",
+    image: home5,
+    buttonText: "Teljes cikk",
+    link: "https://divany.hu/offline/a-nagy-penzrablas-kulisszatitkok-sorozat/"
+  },
+ 
+]
---
+++ src/datas/szereplok.jsx
@@ -0,0 +1,34 @@
+import Helsinki from '../assets/helsinki.jpg'
+import Professor from '../assets/professor.jpg'
+import Tokio from '../assets/Tokio.jpg'
+import Denver from '../assets/denver.jpg'
+export const Szereplok = [
+  {
+    fictiveName: "Tokió",
+    realName: "Úrsula Corberó",
+    age: 36,
+    image: Tokio,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos delectus magnam, voluptas modi harum doloribus minima aliquam error ullam!",
+  },
+  {
+    fictiveName: "Professzor",
+    realName: "Álvaro Morte",
+    age: 47,
+    image: Professor,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos delectus magnam, voluptas modi harum doloribus minima aliquam error ullam!",
+  },
+  {
+    fictiveName: "Helsinki",
+    realName: "Darko Perić ",
+    age: 45,
+    image: Helsinki,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos delectus magnam, voluptas modi harum doloribus minima aliquam error ullam!",
+  },
+  {
+    fictiveName: "Denver",
+    realName: "Jamie Lorente ",
+    age: 29,
+    image: Denver,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dignissimos delectus magnam, voluptas modi harum doloribus minima aliquam error ullam!",
+  },
+]
